6f5cd84d730613a3db87c36a8c55cb57109c82b2
Sergicii <quisejo5@hotmail.com>
2025-07-01 22:50:33 -0500

    agregando estilos y colores al css

--- src/main/java/gatodev/pa4web/config/DBConnector.java
+++ src/main/java/gatodev/pa4web/config/DBConnector.java
@@ -11,7 +11,7 @@ public class DBConnector {
 
     //Credenciales
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "root";
     private static final String URL = "jdbc:mysql://localhost:3306/";
     private static final String DB_NAME = "karate_league";
 
