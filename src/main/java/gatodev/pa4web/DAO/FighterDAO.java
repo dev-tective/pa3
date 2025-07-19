45b4f12e3dfd575724a84bc19567eff104412a61
Alberto43342 <nfcoral72@gmail.com>
2025-07-18 20:24:35 -0500

    Update FighterDAO.java

--- src/main/java/gatodev/pa4web/DAO/FighterDAO.java
+++ src/main/java/gatodev/pa4web/DAO/FighterDAO.java
@@ -12,143 +12,115 @@ public class FighterDAO implements DAO<Fighter, Integer> {
     private final Connection con = DBConnector.dbConnector.getCon();
     public final static FighterDAO instance = new FighterDAO();
 
-    //Siempre instanciar primero academy al iniciar el programa
     private FighterDAO() {
         String sql = """
-                CREATE TABLE IF NOT EXISTS fighters (
-                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                    full_name VARCHAR(60),
-                    age INT,
-                    weight VARCHAR(10),
-                    gender VARCHAR(10),
-                    ranked VARCHAR(15),
-                    modality VARCHAR(20),
-                    id_academy BIGINT REFERENCES academies
-                );
-                """;
-
-        try(Statement smt = con.createStatement()) {
+            CREATE TABLE IF NOT EXISTS fighters (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                dni VARCHAR(15) NOT NULL UNIQUE,
+                full_name VARCHAR(60) NOT NULL,
+                age INT NOT NULL,
+                weight DOUBLE NOT NULL,
+                gender VARCHAR(10) NOT NULL,
+                rank VARCHAR(15) NOT NULL,
+                modality VARCHAR(20) NOT NULL,
+                id_academy INT NOT NULL,
+                id_league INT NOT NULL,
+                photo VARCHAR(255),
+                FOREIGN KEY (id_academy) REFERENCES academies(id),
+                FOREIGN KEY (id_league) REFERENCES leagues(id)
+            );
+            """;
+        try (Statement smt = con.createStatement()) {
             smt.execute(sql);
-        }
-        catch(SQLException e) {
-            throw new RuntimeException(e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage(), e);
         }
     }
 
     @Override
-    public Optional<Fighter> save(Fighter fighter) throws SQLException {
+    public Optional<Fighter> save(Fighter f) throws SQLException {
         String query = """
-                INSERT INTO fighters (
-                    full_name,
-                    age,
-                    weight,
-                    gender,
-                    ranked,
-                    modality,
-                    id_academy
-                )
-                VALUES (?,?,?,?,?,?,?);
-                """;
-
+            INSERT INTO fighters (
+                dni, full_name, age, weight, gender,
+                rank, modality, id_academy, id_league, photo
+            ) VALUES (?,?,?,?,?,?,?,?,?,?);
+        """;
         PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-        setFighter(fighter, ps);
-
+        setFighter(ps, f);
         if (ps.executeUpdate() == 0) return Optional.empty();
-
         ResultSet rs = ps.getGeneratedKeys();
-        if (rs.next()) fighter.setId(rs.getInt(1));
-
-        return Optional.of(fighter);
+        if (rs.next()) f.setId(rs.getInt(1));
+        return Optional.of(f);
     }
 
     @Override
-    public Optional<Fighter> update(Fighter fighter) throws SQLException {
+    public Optional<Fighter> update(Fighter f) throws SQLException {
         String query = """
-                UPDATE fighters SET
-                    full_name = ?,
-                    age = ?,
-                    weight = ?,
-                    gender = ?,
-                    ranked = ?,
-                    modality = ?,
-                    id_academy = ?
-                WHERE id = ?;
-                """;
-
+            UPDATE fighters SET
+                dni=?, full_name=?, age=?, weight=?, gender=?,
+                rank=?, modality=?, id_academy=?, id_league=?, photo=?
+            WHERE id = ?;
+        """;
         PreparedStatement ps = con.prepareStatement(query);
-        setFighter(fighter, ps);
-        ps.setInt(8, fighter.getId());
-
-        return ps.executeUpdate() == 0 ? Optional.empty() : Optional.of(fighter);
+        setFighter(ps, f);
+        ps.setInt(11, f.getId());
+        if (ps.executeUpdate() == 0) return Optional.empty();
+        return Optional.of(f);
     }
 
-    private void setFighter(Fighter fighter, PreparedStatement ps) throws SQLException {
-        ps.setString(1, fighter.getFullName());
-        ps.setInt(2, fighter.getAge());
-        ps.setString(3, fighter.getWeight());
-        ps.setString(4, fighter.getGender());
-        ps.setString(5, fighter.getRank());
-        ps.setString(6, fighter.getModality());
-        ps.setInt(7, fighter.getIdAcademy());
+    private void setFighter(PreparedStatement ps, Fighter f) throws SQLException {
+        ps.setString(1, f.getDni());
+        ps.setString(2, f.getFullName());
+        ps.setInt(3, f.getAge());
+        ps.setDouble(4, f.getWeight());
+        ps.setString(5, f.getGender());
+        ps.setString(6, f.getRank());
+        ps.setString(7, f.getModality());
+        ps.setInt(8, f.getIdAcademy());
+        ps.setInt(9, f.getIdLeague());
+        ps.setString(10, f.getPhoto());
     }
 
     @Override
     public boolean deleteById(Integer id) throws SQLException {
-        String query = """
-                DELETE FROM fighters WHERE id = ?;
-                """;
-
-        PreparedStatement ps = con.prepareStatement(query);
+        PreparedStatement ps = con.prepareStatement("DELETE FROM fighters WHERE id = ?");
         ps.setInt(1, id);
         return ps.executeUpdate() != 0;
     }
 
     @Override
     public List<Fighter> findAll() throws SQLException {
-        List<Fighter> fighters = new ArrayList<>();
-        String query = """
-                SELECT * FROM fighters;
-                """;
-
-        PreparedStatement ps = con.prepareStatement(query);
+        List<Fighter> list = new ArrayList<>();
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM fighters;");
         ResultSet rs = ps.executeQuery();
-
         while (rs.next()) {
-            fighters.add(Fighter.builder()
-                    .id(rs.getInt("id"))
-                    .fullName(rs.getString("full_name"))
-                    .age(rs.getInt("age"))
-                    .weight(rs.getString("weight"))
-                    .gender(rs.getString("gender"))
-                    .rank(rs.getString("ranked"))
-                    .modality(rs.getString("modality"))
-                    .idAcademy(rs.getInt("id_academy"))
-                    .build());
+            list.add(map(rs));
         }
-
-        return fighters;
+        return list;
     }
 
     @Override
     public Optional<Fighter> findById(Integer id) throws SQLException {
-        String query = """
-                SELECT * FROM fighters WHERE id = ?;
-                """;
-
-        PreparedStatement ps = con.prepareStatement(query);
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM fighters WHERE id = ?");
         ps.setInt(1, id);
-
         ResultSet rs = ps.executeQuery();
+        return rs.next() ? Optional.of(map(rs)) : Optional.empty();
+    }
 
-        return !rs.next() ? Optional.empty() : Optional.of(Fighter.builder()
-                .id(rs.getInt("id"))
-                .fullName(rs.getString("full_name"))
-                .age(rs.getInt("age"))
-                .weight(rs.getString("weight"))
-                .gender(rs.getString("gender"))
-                .rank(rs.getString("ranked"))
-                .modality(rs.getString("modality"))
-                .idAcademy(rs.getInt("id_academy"))
-                .build());
+    private Fighter map(ResultSet rs) throws SQLException {
+        return Fighter.builder()
+            .id(rs.getInt("id"))
+            .dni(rs.getString("dni"))
+            .fullName(rs.getString("full_name"))
+            .age(rs.getInt("age"))
+            .weight(rs.getDouble("weight"))
+            .gender(rs.getString("gender"))
+            .rank(rs.getString("rank"))
+            .modality(rs.getString("modality"))
+            .idAcademy(rs.getInt("id_academy"))
+            .idLeague(rs.getInt("id_league"))
+            .photo(rs.getString("photo"))
+            .build();
     }
 }
+
