cd2bd5fa440541b21a958e024e1affe405a7b2a6
Alberto43342 <nfcoral72@gmail.com>
2025-07-18 20:23:39 -0500

    Update FighterService.java

--- src/main/java/gatodev/pa4web/services/FighterService.java
+++ src/main/java/gatodev/pa4web/services/FighterService.java
@@ -3,20 +3,18 @@ package gatodev.pa4web.services;
 import gatodev.pa4web.DTO.FighterDTO;
 import gatodev.pa4web.models.Academy;
 import gatodev.pa4web.models.Fighter;
+import gatodev.pa4web.models.League;
 
 import java.util.List;
 
 public interface FighterService {
 
-    Fighter addFighter(Fighter fighter);
-
-    Fighter updateFighter(Fighter fighter);
-
+    Fighter addFighter(Fighter f);
+    Fighter updateFighter(Fighter f);
     boolean deleteFighter(Integer id);
-
     Fighter getFighter(Integer id);
-
     List<Fighter> getAllFighters();
 
-    FighterDTO convertToFighterDTO(Fighter fighter, Academy academy);
+    FighterDTO convertToFighterDTO(Fighter f, Academy academy, League league);
 }
+
