a8d92c7997adc39a837d6f672b04674051072849
Alberto43342 <nfcoral72@gmail.com>
2025-07-18 20:20:09 -0500

    Update Fighter.java

--- src/main/java/gatodev/pa4web/models/Fighter.java
+++ src/main/java/gatodev/pa4web/models/Fighter.java
@@ -1,21 +1,22 @@
 package gatodev.pa4web.models;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
-@Builder
-@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fighter {
     private Integer id;
+    private String dni;
     private String fullName;
     private Integer age;
-    private String weight;
+    private Double weight;
     private String gender;
-    private String rank;
-    private String modality;
-    private Integer idAcademy;
+    private String rank;        
+    private String modality;    
+    private Academy academy;     
+    private League league;      
+    private String photo;      
 }
