62483136703762e3e760bf66163a1b9d1d75bf6a
Alberto43342 <nfcoral72@gmail.com>
2025-07-18 20:13:19 -0500

    Create LogoutServlet.java

---
+++ src/main/java/gatodev/pa4web/controllers/LogoutServlet.java
@@ -0,0 +1,24 @@
+package gatodev.pa4web.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Invalidar
+        request.getSession().invalidate();
+
+        // Redirigir al login
+        response.sendRedirect(request.getContextPath() + "/login");
+    }
+}
