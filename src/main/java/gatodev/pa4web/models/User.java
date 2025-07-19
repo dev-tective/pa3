6fa7d4c16426b1ee5f40f23c1743514c26e1d158
Alberto43342 <nfcoral72@gmail.com>
2025-07-18 20:04:53 -0500

    Create User.java

---
+++ src/main/java/gatodev/pa4web/models/User.java
@@ -0,0 +1,16 @@
+package gatodev.pa4web.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String role; 
+}
